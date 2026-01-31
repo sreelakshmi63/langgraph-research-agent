6f80536c71562f1901c243ac3c64295457cf3cf4
reboundxlove <reboundxlove@gmail.com>
2026-02-01 03:02:55 +0530

    duplication removed

--- run_agent.py
+++ run_agent.py
@@ -34,39 +34,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-import sys
-from langchain_core.messages import HumanMessage
-from graph import app
-
-from dotenv import load_dotenv
-load_dotenv()
-
-def main():
-    if len(sys.argv) < 2:
-        print('WRONG SYNTAX! Please check the usage: python run_agent.py "your question"')
-        return
-
-    question = sys.argv[1]
-
-    print("\n🧠 Question:")
-    print(question)
-    print("\n🤖 Agent thinking...\n")
-
-    result = app.invoke(
-        {
-            "messages": [HumanMessage(content=question)],
-             "steps": 0
-        },
-        config={"recursion_limit": 100}
-    )
-
-    print("\n✅ Final Answer:")
-    # print(result["messages"][-1].content)
-    print(result)
-    print("======================")
-    print("======================")
-    print("======================")
-    print(result["messages"][-1])
-
-if __name__ == "__main__":
-    main()
